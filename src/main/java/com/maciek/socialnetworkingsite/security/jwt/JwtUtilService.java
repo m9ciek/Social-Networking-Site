d904544eb39503909f15631a3584dad2180d7387
m9ciek <ahubu123@gmail.com>
2020-07-31 18:39:45 +0200

    Added externalized config for JWT

--- src/main/java/com/maciek/socialnetworkingsite/security/jwt/JwtUtilService.java
+++ src/main/java/com/maciek/socialnetworkingsite/security/jwt/JwtUtilService.java
@@ -3,31 +3,41 @@ package com.maciek.socialnetworkingsite.security.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 @Service
 public class JwtUtilService {
 
-    private static final String SECRET_KEY = "secret";
+    @Value("${jwt.token.secret}")
+    private String secretKey;
+
+    @Value("${jwt.token.expiration}")
+    private long expirationTime;
 
     public String createToken(UserDetails userDetails) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime exp = LocalDateTime.now().plusSeconds(expirationTime);
+
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-                .signWith(SignatureAlgorithm.HS512, SECRET_KEY).compact();
+                .setIssuedAt(Date.from(now.atZone(ZoneId.systemDefault()).toInstant()))
+                .setExpiration(Date.from(exp.atZone(ZoneId.systemDefault()).toInstant()))
+                .signWith(SignatureAlgorithm.HS512, secretKey).compact();
     }
 
     public String extractUsername(String token) {
-        Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
         return claims.getSubject();
     }
 
     public Date extractExpirationDate(String token) {
-        Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
         return claims.getExpiration();
     }
 
